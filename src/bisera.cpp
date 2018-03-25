3575cbd932307af3c0e835d7350ebe3872352f26
Daniel <dackerson0@gmail.com>
2018-03-25 17:46:23 -0400

    Initial commit

---
+++ src/bisera.cpp
@@ -0,0 +1,10 @@
+/*  bisera.cpp
+ *  This is the main file for the game Bisera
+ */
+
+#include "game.h"
+
+int main() {
+    Game g;
+    return 0;
+}
---
+++ src/game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+public:
+    Game();
+    ~Game();
+private:
+};
+
+#endif//GAME_H
