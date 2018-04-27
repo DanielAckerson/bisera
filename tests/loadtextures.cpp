b62544791377beb51828dcf714d3f191a2a6dc86
Daniel Ackerson <dackerson0@gmail.com>
2018-04-26 21:35:06 -0400

    added demo for loading textures. now using git lfs to track png

---
+++ tests/loadtextures.cpp
@@ -0,0 +1,274 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <lodepng.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+float vertices[] = {
+//  Position----  Texcoords-
+    -0.5f,  0.5f, 0.0f, 0.0f, // 0 Top-left
+     0.5f,  0.5f, 1.0f, 0.0f, // 1 Top-right
+     0.5f, -0.5f, 1.0f, 1.0f, // 2 Bottom-right
+    -0.5f, -0.5f, 0.0f, 1.0f  // 3 Bottom-left
+};
+
+
+// these refer to the vertex array
+GLuint elements[] = {
+    0, 1, 2,    // Top-left triangle
+    2, 3, 0     // Bottom-right triangle
+};
+
+
+GLuint colors[] = {
+    0xffff00ff, 0xff000000,
+    0xff000000, 0xffff00ff
+};
+
+
+bool createContext(GLFWwindow **window, const char *title, int width, int height);
+GLuint loadTexture(const char *filename);
+GLuint loadTexture(GLuint *data, GLint width, GLint height);
+GLuint loadShader(const char *vertex_path, const char *fragment_path);
+
+
+int main() {
+//create window
+    GLFWwindow *window;
+    if(!createContext(&window, "loadtextures", 1280, 720)) {
+        return 1;
+    }
+
+//create Vertex Array Object; it holds info about gl buffers (i think)
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+//load vertices
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+//Create an element array (the triangles)
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+//load shader program
+    GLuint program = loadShader(
+        "assets/loadtextures.vs",
+        "assets/loadtextures.fs");
+
+//load textures
+    GLuint checkered_texID = loadTexture("assets/loadtextures_chepckered.png");
+    /* GLuint checkered_texID = loadTexture(colors, 2, 2); */
+    glUniform1i(glGetUniformLocation(program, "myTextureSampler"), 0);
+
+//link data from vertices[] to shader program
+    GLint posAttrib = glGetAttribLocation(program, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), 0);
+
+    GLint texAttrib = glGetAttribLocation(program, "vertexUV");
+    glEnableVertexAttribArray(texAttrib);
+    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), (void*)(2*sizeof(float)));
+
+        glUseProgram(program);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+//render
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //draws currently bound element array buffer
+        glfwSwapBuffers(window);
+    }
+
+//cleanup
+    glDeleteTextures(1, &checkered_texID);
+    glDeleteProgram(program);
+    glDeleteBuffers(1, &ebo);
+    glDeleteBuffers(1, &vbo);
+
+    glDeleteVertexArrays(1, &vao);
+
+    glfwTerminate();
+    return 0;
+}
+
+
+bool createContext(GLFWwindow **window, const char *title, int width, int height) {
+    if(!glfwInit()) {
+        cerr << "error: failed to init glfw" << std::endl;
+        return false;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    *window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if(!*window) {
+        glfwTerminate();
+        cerr << "error: failed to create window";
+        return false;
+    }
+    glfwMakeContextCurrent(*window);
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        glfwTerminate();
+        cerr << "error: failed to load OpenGL with glad";
+        return false;
+    }
+    // The official code for "Setting Your Raster Position to a Pixel Location" (i.e. set up a camera for 2D screen)
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, width, height, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Make some OpenGL properties better for 2D and enable alpha channel.
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glDisable(GL_ALPHA_TEST);
+    cout<<"OpenGL " << glGetString(GL_VERSION) << ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    return true;
+}
+
+
+GLuint loadTexture(const char *filename) {
+    GLuint texID;
+    std::vector<unsigned char> image;
+    unsigned int width, height;
+    unsigned int error = lodepng::decode(image, width, height, filename);
+    if(error != 0) {
+        cout << "error " << error << ": " << lodepng_error_text(error) << endl;
+        return 1;
+    }
+    cout << "image dim: " << width << ", " << height << endl;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA,
+        width,
+        height,
+        0,
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        image.data());
+    return texID;
+}
+
+
+GLuint loadTexture(GLuint *data, GLint width, GLint height) {
+    GLuint texID;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    cout << "image dim: " << width << ", " << height << endl;
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA,
+        width,
+        height,
+        0,
+        GL_RGBA,
+        GL_UNSIGNED_INT_8_8_8_8_REV,
+        data);
+    return texID;
+}
+
+
+std::string readFile(const char *filePath) {
+    std::string content;
+    std::ifstream fileStream(filePath, std::ios::in);
+
+    if(!fileStream.is_open()) {
+        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+        return "";
+    }
+
+    std::string line = "";
+    while(!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    fileStream.close();
+    return content;
+}
+
+
+GLuint loadShader(const char *vertex_path, const char *fragment_path) {
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Read shaders
+    std::string vertShaderStr = readFile(vertex_path);
+    std::string fragShaderStr = readFile(fragment_path);
+    const char *vertShaderSrc = vertShaderStr.c_str();
+    const char *fragShaderSrc = fragShaderStr.c_str();
+
+    GLint result = GL_FALSE;
+    int logLength;
+
+    // Compile vertex shader
+    std::cout << "Compiling vertex shader." << std::endl;
+    glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
+    glCompileShader(vertShader);
+
+    // Check vertex shader
+    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> vertShaderError((logLength > 1) ? logLength : 1);
+    glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
+    std::cout << &vertShaderError[0] << std::endl;
+
+    // Compile fragment shader
+    std::cout << "Compiling fragment shader." << std::endl;
+    glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
+    glCompileShader(fragShader);
+
+    // Check fragment shader
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> fragShaderError((logLength > 1) ? logLength : 1);
+    glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
+    std::cout << &fragShaderError[0] << std::endl;
+
+    std::cout << "Linking program" << std::endl;
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertShader);
+    glAttachShader(program, fragShader);
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &result);
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> programError( (logLength > 1) ? logLength : 1 );
+    glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
+    std::cout << &programError[0] << std::endl;
+
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
+
+    return program;
+}
