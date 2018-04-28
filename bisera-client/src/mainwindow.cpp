2fe4741d4bf744fd036081e472ffe6825d5c3767
Daniel Ackerson <dackerson0@gmail.com>
2018-04-27 23:30:54 -0400

    added essential code for loading OpenGL

--- bisera-client/src/mainwindow.cpp
+++ bisera-client/src/mainwindow.cpp
@@ -8,6 +8,11 @@ MainWindow::MainWindow()
     if(!glfwInit()) {
         throw "error: failed to init glfw";
     }
+
+    // required to get up-to-date OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // set monitor for fullscreen
     // for borderless window, see http://www.glfw.org/docs/latest/window_guide.html#window_windowed_full_screen
     /* monitor = glfwGetPrimaryMonitor(); */
@@ -24,7 +29,22 @@ MainWindow::MainWindow()
         glfwTerminate();
         throw "error: failed to load OpenGL with glad";
     }
+    // The official code for "Setting Your Raster Position to a Pixel Location" (i.e. set up a camera for 2D screen)
     glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, width, height, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Make some OpenGL properties better for 2D and enable alpha channel.
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glDisable(GL_ALPHA_TEST);
+
+    std::cout<<"OpenGL " << glGetString(GL_VERSION) << ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
     /* Set user pointer to reference this instance from static calls*/
     /* this is useless. maybe point to input handler instead */
     //glfwSetWindowUserPointer(window, this);
