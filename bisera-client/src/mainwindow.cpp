ab30b3dc9ac7ba0a2fff7f009af1491a9aebeead
Daniel Ackerson <dackerson0@gmail.com>
2018-05-20 13:21:15 -0400

    added missing scope specifier

--- bisera-client/src/mainwindow.cpp
+++ bisera-client/src/mainwindow.cpp
@@ -4,31 +4,33 @@
 
 MainWindow::MainWindow()
 :   window(nullptr), monitor(nullptr), width(1280), height(720) {
-    /* Initialize glfw */
+    // Initialize glfw
     if(!glfwInit()) {
         throw "error: failed to init glfw";
     }
 
-    // required to get up-to-date OpenGL
+    // Required to get up-to-date OpenGL
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // set monitor for fullscreen
-    // for borderless window, see http://www.glfw.org/docs/latest/window_guide.html#window_windowed_full_screen
+    // Set monitor for fullscreen
+    // For borderless window, see http://www.glfw.org/docs/latest/window_guide.html#window_windowed_full_screen
     /* monitor = glfwGetPrimaryMonitor(); */
 
-    /* Create a windowed mode window and its OpenGL context */
+    // Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(width, height, "Bisera", monitor, NULL);
     if(!window) {
         glfwTerminate();
         throw "error: failed to create window";
     }
-    /* Make the window's context current */
+
+    // Make the window's context current
     glfwMakeContextCurrent(window);
     if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         glfwTerminate();
         throw "error: failed to load OpenGL with glad";
     }
+
     // The official code for "Setting Your Raster Position to a Pixel Location" (i.e. set up a camera for 2D screen)
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
@@ -44,10 +46,11 @@ MainWindow::MainWindow()
     glEnable(GL_BLEND);
     glDisable(GL_ALPHA_TEST);
 
-    std::cout<<"OpenGL " << glGetString(GL_VERSION) << ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
-    /* Set user pointer to reference this instance from static calls*/
-    /* this is useless. maybe point to input handler instead */
-    //glfwSetWindowUserPointer(window, this);
+    std::cout << "OpenGL " << glGetString(GL_VERSION)
+        << ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    // Set user pointer to reference this instance from static calls
+    // this is useless. maybe point to input handler instead
+    /* glfwSetWindowUserPointer(window, this); */
     std::cout << "MainWindow created!" << std::endl;
 }
 
